524d4be60697364e379a004d46a73ef229730cc4
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-02 00:44:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# Your Code Here
+
+def map(source_array)
+  index = 0
+  while index < source_array.length do
+    base = source_array[index]
+    yield(base)
+    index += 1
+  end
+  map([1, 2, 3, -9]) do |x|
+x * -1
+end
+end
+
+# def reduce(source_array, starting_value = nil)
+#   startint_index = starting_value ? 0 : 1
+#   starting_value ||= source_array[0]
+#   source_array[startint_index..-1].each do |el| 
+#     # p starting_value || el
+#     starting_value = yield(starting_value, el)
+    
+#   end
+#   starting_value
+  
+# end
+
+
+
